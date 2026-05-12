177744f0d1eed3fb61940f5a51120c5d5d268e34
Dev Chhangani <dev.chhangani1@gmail.com>
2026-05-12 20:43:16 +0530

    first commit

---
+++ starter/script.js
@@ -0,0 +1,119 @@
+/* let name = prompt('What is your name?');
+let num1 = prompt('enter num 1');
+let num2 = prompt('enter num 2');
+console.log(num1+num2); 
+let PI = 3.1415
+*/
+
+/* assignmen9t 1 
+ 
+let country = 'India';
+let continent = 'Asia';
+let population = '80';
+
+console.log(country);
+console.log(continent);
+console.log(population);*/
+
+ // data types//
+/*
+let javascriptIsFun = true;
+console.log (javascriptIsFun);
+
+//console.log (typeof true);
+console.log (typeof javascriptIsFun);
+//console.log (typeof 23);
+//console.log (typeof 'dev');
+
+javascriptIsFun = "YES!"
+console.log(typeof javascriptIsFun) ;
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2008;
+console.log(typeof year);
+
+console.log(typeof null)
+
+
+ let age = 30;
+ age = 32;
+
+const birthYear = 1991;
+ birthYear = 19909;
+
+ const job;
+
+ var job = 'programmer';
+ job = 'teacher'
+
+LastName = 'Chhangani';
+console.log(LastName);
+
+
+const now = 2026;
+const agedev = now-2008;
+const agepuneet = now-1991;
+console.log(agedev,agepuneet);
+
+console.log (agedev * 99,agepuneet / 99,2 ** 3)
+// 2 ** 3 means to the power of 3 = 2 * 2 * 2
+
+const firstName = 'dev';
+const LastName = 'chhangani';
+console.log(firstName +' '+ LastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4 = 100
+x ++; // x = x + 1 = 101
+x --; // x= = x - 1 = 100
+console.log (x);
+
+// comparison operators
+
+console.log(agedev,agepuneet);
+console.log(agedev > agepuneet); // >, <, >=, <=
+console.log(agedev >= 18);
+
+const isfullage = agedev >= 18;
+
+
+const now = 2026;
+const agedev = now-2008;
+const agepuneet = now-1991;
+
+console.log ( now - 2008 < now - 1991);
+
+let x, y;
+x = y = 25 - 10 - 5; // y = x = 10
+console.log (x,y);
+
+const averageAge = (agedev + agepuneet) / 2
+console.log(agedev, agepuneet, averageAge);
+
+JavaScript Fundamentals – Part 
+
+Coding Challenge #1
+
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK 
+*/
+
